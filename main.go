85dbebc8eb95ddcfbad64ef6b9c4f5e3c074187b
ddddyyyy <2115601599@qq.com>
2020-11-13 16:30:01 +0800

    更新装备掉落地图限制

--- main.go
+++ main.go
@@ -141,7 +141,7 @@ func getSortedKey(param Param, cache map[string]int) *ValueSorter {
 					tV, _ := strconv.ParseInt(value[:len(value)-1], 10, 64)
 					tV = tV * int64(cache[equipment.Title])
 					tK := strings.Replace(key, "\t", "", -1) //地图名
-					if strings.Contains(tK, "36") {
+					if strings.Contains(tK, "37") || strings.Contains(tK, "38") || strings.Contains(tK, "39") {
 						continue
 					}
 					if strings.Contains(tK, "VH") || strings.Contains(tK, "HARD") {
