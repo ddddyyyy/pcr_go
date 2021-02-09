d0d475c4fc3f66fa13307d9d50e277c18261fb45
ddddyyyy <2115601599@qq.com>
2021-02-09 14:44:34 +0800

    开放39图

--- main.go
+++ main.go
@@ -144,9 +144,6 @@ func getSortedKey(param Param, cache map[string]int) *ValueSorter {
 					tV, _ := strconv.ParseInt(value[:len(value)-1], 10, 64)
 					tV = tV + int64(cache[equipment.Title])
 					tK := strings.Replace(key, "\t", "", -1) //地图名
-					if strings.Contains(tK, "39") {
-						continue
-					}
 					if strings.Contains(tK, "VH") || strings.Contains(tK, "HARD") {
 						//优先度低于普通地图
 						if result[tK] == 0 {
